815110fab761caba0c250dbad5012d6406a418e8
Stella Kim <stella@Stellas-MacBook-Pro.local>
2019-09-19 16:26:52 -0700

    add comments, organize functions, update html

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,68 +1,66 @@
-$("document").ready(function () {
-    console.log("Ready!");
+// Set global variables for the game.
+var targetNumber;
+var userNumber;
+var crystalValue;
+var numberCrystals;
+var endGame;
+var wins = 0;
+var losses = 0;
 
+// Create a function to start the game.
+function startGame() {
     // Create a random number generator to pick between a value between 19 and 120.
-    var targetNumber = Math.floor((Math.random() * 120) + 19);
-    var userNumber = 0;
-    var wins = 0;
-    var losses = 0;
+    targetNumber = Math.floor((Math.random() * 120) + 19);
+    userNumber = 0;
+    endGame = false;
+
+    // Create a variable to hold the number of crystals based on images shown on the page.
+    numberCrystals = $(".crystal-image").length;
+    console.log("Number of crystals: " + numberCrystals);
+
+    $('.crystal-image').each(function (i) {
+        // Generate a random number for each crystal with a value between 1 and 12.
+        crystalValue = Math.floor((Math.random() * 12) + 1);
+        console.log(crystalValue);
+        $(this).attr("dataValue", crystalValue);
+        console.log("Set crystal " + i + " to " + crystalValue);
+    });
 
     // Display the user's score and random target number.
     $("#target-number").text(targetNumber);
-    console.log(targetNumber);
+    console.log("Target number: " + targetNumber);
 
     $("#score-number").text(userNumber);
-    console.log(userNumber);
+    console.log("User score: " + userNumber);
+};
 
-    // Set variables for each crystal's value as well as the number of crystals based on images.
-    var crystalValue;
-    var numberCrystals = $(".crystal-image").length;
-    console.log(numberCrystals);
+// Create parameters for winning or losing the game based on the user's click.
+$(".crystal-image").on("click", function () {
+    console.log("test onclick");
+    if (userNumber < targetNumber) {
+        crystalValue = $(this).attr("dataValue");
+        crystalValue = parseInt(crystalValue);
+        userNumber += crystalValue;
+        $("#score-number").text(userNumber);
+    }
 
-    // Create a function to start the game.
-    function startGame() {
-        $('.crystal-image').each(function (i) {
-            // Generate a random number for each crystal with a value between 1 and 12.
-            crystalValue = Math.floor((Math.random() * 12) + 1);
-            console.log(crystalValue);
-            $(this).attr("dataValue", crystalValue);
-            console.log("Set crystal " + i + " to " + crystalValue);
-        });
+    if (endGame) {
+        startGame();
+    }
 
-        $(".crystal-image").on("click", function () {
-            crystalValue = $(this).attr("dataValue");
-            crystalValue = parseInt(crystalValue);
-            userNumber += crystalValue;
-            console.log(userNumber);
-            $("#score-number").text(userNumber);
-        });
-
-        if (targetNumber === userNumber) {
-            wins++;
-            youWin();
-            $(".wins").text(wins);
-        }
-
-        else if (userNumber > targetNumber) {
-            losses++;
-            console.log(losses);
-            $(".losses").text(losses);
-            youLose();
-        }
-
-    };
-
-    // Create a function for when the user wins the game.
-    function youWin() {
+    else if (targetNumber === userNumber) {
+        wins++;
+        $("#wins").text(wins);
+        endGame = true;
         alert("You win!");
     }
 
-    // Create a function for when the user loses the game.
-    function youLose() {
-        alert("You lose!");
+    else if (userNumber > targetNumber) {
+        losses++;
+        $("#losses").text(losses);
+        endGame = true;
+        alert("Sorry, better luck next time...");
     }
-
-
-    startGame();
-
 });
+
+$("document").ready(startGame);
